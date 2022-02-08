6ff66e9c87187cb1773f10c3925392d2a9ad82af
MuhammadHamzaFarooq <mhamza2021999@gmail.com>
2022-02-08 23:33:04 +0500

    Initial commit

---
+++ Dad Joke Chrome Extension/script.js
@@ -0,0 +1,6 @@
+fetch("https://icanhazdadjoke.com/slack").then(data=>data.json()).then((jokeData)=>{
+  const jokeText =jokeData.attachments[0].text;
+  const jokeElem = document.getElementById('jokeElem');
+  jokeElem.innerHTML = jokeText
+})
+
